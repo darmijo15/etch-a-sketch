0c119479144c900fbef5fc98b3bf1f67cdd046a0
darmijo15 <daniel.armijo@wsu.edu>
2022-03-22 23:30:00 -0700

    add a reset button that clears the canvas and sends a prompt asking the user to choose the dimension of the canvas

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+const resetBtn = document.querySelector('#reset');
 
 for (i = 1; i <= 256; i++) {
     const square = document.createElement('div');
@@ -9,3 +10,32 @@ for (i = 1; i <= 256; i++) {
         square.style.backgroundColor = '#000000';
     })
 }
+
+resetBtn.addEventListener('click', function() {
+
+    container.innerHTML = '';
+
+    let input = prompt('How many squares do you want on each side of the grid?', '16');
+
+    if (input > 100) {
+        alert('Too big, please select a smaller number.');
+    } else {
+
+    const gridSize = input * input;
+    const squareSize = 608 / input;
+
+    for (i = 1; i <= gridSize; i++) {
+        const square = document.createElement('div');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        square.style.backgroundColor = '#ffffff';
+        container.appendChild(square);
+
+        square.addEventListener('mouseover', function() {
+            square.style.backgroundColor = '#000000';
+        })
+
+    }
+    }
+})
+
