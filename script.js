c379b8b9773b956a943767eb3f89679d7e95eba6
darmijo15 <daniel.armijo@wsu.edu>
2022-03-22 21:24:48 -0700

    add an eventListener to every square div created in the for loop that changes the background color of the square to black

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const container = document.querySelector('.container');
 
 for (i = 1; i <= 256; i++) {
-const square = document.createElement('div');
-square.classList.add('square');
-container.appendChild(square);
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+
+    square.addEventListener('mouseover', function() {
+        square.style.backgroundColor = '#000000';
+    })
 }
