54bef37e5897022bd21c028dc7f06126acce4bd8
darmijo15 <daniel.armijo@wsu.edu>
2022-03-23 21:10:07 -0700

    refactor the code to utilize global variables (for the input, gridSize, and squareSize) and a createGrid() function

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
 const container = document.querySelector('.container');
 const resetBtn = document.querySelector('#reset');
 
-for (i = 1; i <= 256; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+let input = 16;
+let gridSize = input * input;
+let squareSize = 640 / input;
 
-    square.addEventListener('mouseover', function() {
-        square.style.backgroundColor = '#000000';
-    })
-}
-
-resetBtn.addEventListener('click', function() {
-
-    container.innerHTML = '';
-
-    let input = prompt('How many squares do you want on each side of the grid?', '16');
-
-    if (input > 100) {
-        alert('Too big, please select a smaller number.');
-    } else {
-
-    const gridSize = input * input;
-    const squareSize = 608 / input;
-
+function createGrid() {
     for (i = 1; i <= gridSize; i++) {
         const square = document.createElement('div');
+
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
         square.style.backgroundColor = '#ffffff';
+
         container.appendChild(square);
 
-        square.addEventListener('mouseover', function() {
+        square.addEventListener('mouseover', function () {
             square.style.backgroundColor = '#000000';
         })
-
     }
+    console.log('test');
+}
+
+createGrid();
+
+resetBtn.addEventListener('click', function() {
+    input = prompt('How many squares do you want on each side of the grid?', '16');
+    gridSize = input * input;
+    squareSize = 640 / input;
+
+    if (input > 100) {
+        alert('Please select a smaller number of squares.');
+    } else {
+        container.innerHTML = '';
+        createGrid();
     }
 })
-
