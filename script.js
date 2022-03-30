6a965f3941647c366900f809fcdb2549a383bc13
darmijo15 <daniel.armijo@wsu.edu>
2022-03-29 20:29:10 -0700

    change the class, id, and variable names to be more consistent

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const container = document.querySelector('.container');
+const sketchpadContainer = document.querySelector('.sketchpad-container');
 const clearBtn = document.querySelector('#clear');
 const resizeBtn = document.querySelector('#resize');
 const colorPicker = document.querySelector('#color-picker');
 
 let input = 16;
-let gridSize = input * input;
+let sketchpadSize = input * input;
 let squareSize = 640 / input;
 
 let squareColor = colorPicker.value;
 
-function createGrid() {
-    for (i = 1; i <= gridSize; i++) {
+function createSketchpad() {
+    for (i = 1; i <= sketchpadSize; i++) {
         const square = document.createElement('div');
 
         square.style.width = `${squareSize}px`;
         square.style.height = `${squareSize}px`;
         square.style.backgroundColor = '#ffffff';
 
-        container.appendChild(square);
+        sketchpadContainer.appendChild(square);
 
         square.addEventListener('mouseover', function () {
             square.style.backgroundColor = squareColor;
@@ -25,16 +25,16 @@ function createGrid() {
     }
 }
 
-createGrid();
+createSketchpad();
 
 clearBtn.addEventListener('click', function() {
-    container.innerHTML = '';
-    createGrid();
+    sketchpadContainer.innerHTML = '';
+    createSketchpad();
 })
 
 resizeBtn.addEventListener('click', function() {
 
-    promptInput = prompt('How many squares do you want on each side of the grid?', '16');
+    promptInput = prompt('How many squares do you want on each side of the sketchpad? Choose any number 1-100.', '16');
 
     if (promptInput === null) {
         return;
@@ -44,10 +44,10 @@ resizeBtn.addEventListener('click', function() {
         alert('Please enter a number greater than 0.');
     } else if (promptInput >= 1 && promptInput <= 100) {
         input = promptInput; 
-        gridSize = input * input;
+        sketchpadSize = input * input;
         squareSize = 640 / input;
-        container.innerHTML = '';
-        createGrid();
+        sketchpadContainer.innerHTML = '';
+        createSketchpad();
     } else {
         alert('Please select a smaller number.')
     }
